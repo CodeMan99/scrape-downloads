cb010bc52f3542b81578628f3bb9f5f5a840797a
Cody A. Taylor <codemister99@yahoo.com>
2015-07-16 09:56:04 -0400

    Factor out folder naming.

--- chunk-zip-by-folders.py
+++ chunk-zip-by-folders.py
@@ -11,13 +11,16 @@ def main():
     )
 
     count = math.ceil(len(files) / 30)
-    for folder_name in range(1, count + 1):
-        os.mkdir(str(folder_name))
+    for folder_name in range(count):
+        os.mkdir(determine_folder(folder_name, count))
 
     for num, file in enumerate(files):
         print(file, '->',
-            shutil.move(file, str(num % count + 1))
+            shutil.move(file, determine_folder(num, count))
         )
+
+def determine_folder(index, folder_count):
+     return str(index % folder_count + 1)
 
 if __name__ == "__main__":
     main()
